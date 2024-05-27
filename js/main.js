e0ba6ca92f766452c212242b0e97b21ea9ddc2dd
ayomahansani <ayohansi17@gmail.com>
2024-05-27 15:40:06 +0530

    changed mobile navbar

--- js/main.js
+++ js/main.js
@@ -14,21 +14,17 @@ window.addEventListener("scroll", () => {
 // end header
 
 
-// menu icon
+$("#icon").on('click', function () {
+        $("ul").toggleClass("show");
+    });
 
-const menuIcon = document.querySelector(".menu-icon");
-const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
-const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 
-menuIcon.addEventListener("click", () => {
-    mobileMenuContainer.classList.add("active");
-});
 
-closeIcon.addEventListener("click", () => {
-    mobileMenuContainer.classList.remove("active");
-});
 
-// end menu icon
+
+
+
+
 
 // ----------------------- End Home Page -----------------------
 

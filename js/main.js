f012723af3d9c31cfab99972c88307ed8c73208d
ayomahansani <ayohansi17@gmail.com>
2024-06-14 20:34:04 +0530

    added js for my-cv pdf

--- js/main.js
+++ js/main.js
@@ -89,6 +89,44 @@ function slidefun(n){
 
 
 
+// ----------------------- Start About Me Page CV-----------------------
+
+// Get the modal
+var modal = document.getElementById("cvModal");
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("cvImage");
+
+
+// When the user clicks on the image, open the modal
+img.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+// ----------------------- End About Me Page CV-----------------------
+
+
+
+
 /*
 
 // ----------------------- Start Contact Me Page -----------------------
